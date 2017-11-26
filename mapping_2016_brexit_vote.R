425c4adc3c88ec8c504181beec3ac07c13e50a2c
Todd K. Hartman <tkhartman@gmail.com>
2017-11-26 15:32:20 +0000

    Update mapping_2016_brexit_vote.R

--- mapping_2016_brexit_vote.R
+++ mapping_2016_brexit_vote.R
@@ -1,40 +1,36 @@
-##' ---
-##' title: "Who Brexited? Mapping the 2016 EU Referendum Vote"
-##' author: "Todd K. Hartman"
-##' date: "`r format(Sys.Date())`"
-##' output: "github_document"
-##' ---
-
+---
+title: "Who Brexited? Mapping the 2016 EU Referendum Vote"
+author: "Todd K Hartman"
+date: "Last updated `r format(Sys.Date())`"
+output: "github_document"
+---
+    
 ##' Housekeeping
 ## Load packages via 'pacman' package manager
-pacman::p_load(broom, data.table, ggplot2, googleVis, rgdal)
-  
+pacman::p_load(broom, ggplot2, googleVis, rgdal)
+
 ## Set the working directory
 # setwd("ENTER YOUR WORKING DIRECTORY HERE")
 
-##' 2016 EU Referendum (Brexit) vote share data
+##' 2016 EU Referendum (Brexit) vote share
 ## Download and import brexit data from the web (or get from Github directly)
-url.df <- "http://www.electoralcommission.org.uk/__data/assets/file/0014/212135/EU-referendum-result-data.csv"
-file.df <- basename(url.df)  # Extract the filename
-if (!file.exists(file.df))   # Only download the file if it doesn't already exist in our working directory
-    download.file(url = url.df, destfile = file.df, mode = "wb")
-brexit <- fread(file.df)  # Read the dataset quickly
-head(brexit)  # Check the data
+url.df <- "https://goo.gl/Bk1vq6"
+file.df <- "EU-referendum-result-data.csv"
 
-## Remove Gibraltar from the dataset for mapping purposes
-brexit <- brexit[!(brexit$Area == "Gibraltar"), ]
+## Only download the file if it doesn't exist in the working directory
+if (!file.exists(file.df))
+    download.file(url = url.df, destfile = file.df)
 
-## Create a common identifier for mapping in ggplot
-brexit <- brexit[order(Area_Code)]
-brexit$id <- 0:380
+## Load the brexit data
+brexit <- read.csv(file.df)
+head(brexit)
+
+## Order 'leave' vote share from highest to lowest
+rank <- order(-brexit$Pct_Leave)  # Create vector of rankings 
+brexit <- brexit[rank, ]  # Sort the data by leave rank, highest to lowest
 
 ## Display 'Leave' vote share from highest to lowest
-brexit$Pct_Leave <- as.numeric(brexit$Pct_Leave)  # Percentage of leave vote share by administrative area
-brexit2 <- subset(brexit, select = c("Area", "Pct_Remain", "Pct_Leave"))  # Subset the data
-brexit2 <- brexit2[order(-Pct_Leave), ]  # Display the 'Leave' vote by area
-print(brexit2[order(-Pct_Leave), ], nrows = 382)  # Display the 'Leave' vote by area
-
-bar.plot <- gvisBarChart(brexit2, xvar = "Area", yvar = "Pct_Leave", 
+bar.plot <- gvisBarChart(brexit, xvar = "Area", yvar = "Pct_Leave", 
                          options = list(legend = "none",
                                         vAxes = "[{textStyle:{fontSize: '16'}}]",
                                         chartArea = "{left:250,top:10,bottom:10}",
@@ -49,39 +45,65 @@ cat(bar.plot$html$chart, file = "brexit.bar.plot.html")
 ## Download and import from the web (or get from Github)
 url.shp <- "https://t.co/GYqX2PCqJ0"
 file.zip <- "esri.zip"
+
+## Download the shape file if it's not present
 if (!file.exists(file.zip)) 
     download.file(url = url.shp, destfile = file.zip, mode = "wb")
-file.shp <- grep("shp", unzip(file.zip), ignore.case=TRUE, value=TRUE)  # Unzip the shapefile
-uk.shp <- readOGR(file.shp, ogrListLayers(file.shp)[1])  # Load the shapefile
 
-## Confirm that the brexit data matches the shapefile
-names(uk.shp)  # Get the variables in the shapefile
-sort(brexit$Area_Code) == sort(uk.shp@data$Area_Cd)  # Are the admin areas the same?
+## Unzio and extract the .shp file
+file.shp <- grep("shp", unzip(file.zip), 
+                 ignore.case=TRUE, value=TRUE)
 
-## Convert the raw data to a data.frame for ggplot
-uk.shp@data$id <- rownames(uk.shp@data)  # Create an id to match the brexit data
-uk.f <- tidy(uk.shp)  # Convert to a dataframe
+## Import the .shp file into R
+uk.shp <- readOGR(file.shp, 
+                  ogrListLayers(file.shp)[1])  # Load the shapefile
 
-## If desired, merge the map with brexit data
-# uk.map <- merge(uk.f, brexit, by="id", all.x=TRUE)
-# uk.map <- uk.map[order(uk.map$order), ]  # Order for plotting purposes
-# head(uk.map)
+## Remove Gibraltar
+brexit2 <- brexit[!(brexit$Area == "Gibraltar"), ]
 
-##' Mapping Brexit vote share using ggplot
-map <- ggplot() +
-    geom_map(data = brexit, 
-             aes(map_id = id, fill = Pct_Leave), 
-             map = uk.f) + 
-    expand_limits(x = uk.f$long, y = uk.f$lat) + 
-    scale_fill_distiller(palette = "Spectral") +
-    # scale_fill_gradient(low = "white", high = "#000034") +  Alternative colour option for printing
-    ggtitle("Brexit Map: 2016 EU Referendum Vote Share") + 
-    labs(fill = "Leave (%)") +
-    theme(plot.title = element_text(lineheight = .8, face = "bold"),
-          axis.title.x = element_blank(),
-          axis.text.x = element_blank(),
-          axis.title.y = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks = element_blank())
+## Fix levels to remove 'Gibraltar'
+brexit2 <- droplevels(brexit2)
+
+## Only keep variables needed for mapping
+brexit2 <- subset(brexit2, select = c("Area_Code", "Pct_Leave"))
+
+## Change names to match shape file for merging
+names(brexit2) <- c("Area_Cd", "Pct_Leave")
+
+## Sort the data by Area Code for merging
+ac.order <- order(brexit2$Area_Cd)  # Create ordering id
+brexit2 <- brexit2[ac.order, ]  # Sort the data by ordering id
+
+## Confirm that the Area Code in the brexit data matches the shapefile
+sort(brexit2$Area_Cd) == sort(uk.shp@data$Area_Cd)
+
+## Merge the datasets
+uk.merged <- merge(uk.shp, brexit2, by = "Area_Cd")
+
+## Convert to a data.frame
+uk.df <- uk.merged@data
+
+## Convert the map data to a data.frame for ggplot using the 'broom' package
+uk.map <- tidy(uk.merged)
+
+## Adjust data 'id' so that it matches the map 'id'
+uk.df$id <- uk.df$id - 1 
+
+## Mapping Brexit vote share using ggplot
+map <- ggplot() +  # Create the plot
+    geom_map(data = uk.df,  # Make a map
+             aes(map_id = uk.df$id, fill = uk.df$Pct_Leave),  # Fill with 'leave' vote
+             map = uk.map) +  # Draw the map using converted .shp file
+    expand_limits(x = uk.map$long, y = uk.map$lat) + # Needed to display map
+    scale_fill_distiller(palette = "Spectral") +  # Choose colours
+    # scale_fill_gradient(low = "white", high = "#000034") +  # Colours for printing
+    ggtitle("Brexit Map: 2016 EU Referendum Vote Share") +  # Title
+    labs(fill = "Leave (%)") +  # Legend label
+    theme(plot.title = element_text(lineheight = .8, face = "bold"),  # Bold title
+          axis.title.x = element_blank(),  # Remove x-axis title
+          axis.text.x = element_blank(),  # Remove x-axis labels
+          axis.title.y = element_blank(),  # Remove y-axis title
+          axis.text.y = element_blank(),  # Remove y-axis labels
+          axis.ticks = element_blank())  # Remove axis tick marks
 
 plot(map)
